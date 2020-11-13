f9bc442e3dd92d8ff46750adfe86ede055a46301
Ship <mnshiplet@gmail.com>
2020-11-13 13:40:58 -0700

    remove date

--- components/PostList.js
+++ components/PostList.js
@@ -11,7 +11,6 @@ export default function PostList({ posts }) {
           posts.map((post) => {
             return (
               <li key={post.slug}>
-                {post.frontmatter.date}: {` `}
                 <Link href={{ pathname: `/post/${post.slug}` }}>
                   <a>{post?.frontmatter?.title}</a>
                 </Link>
